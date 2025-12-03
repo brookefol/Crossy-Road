6e49af2f5467692e5a263d7dfd0897e6d6adc39a
Afua678 <afua.anyadike@gmail.com>
2025-12-03 18:32:35 -0500

    Fixed bug

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -4,7 +4,7 @@ public class GameEvents : MonoBehaviour
 {
     // Creates delegate
     public delegate void Action();
-    public static ifSpacePressed;
+    public static Action ifSpacePressed;
 
     // Update is called once per frame
     void Update()
