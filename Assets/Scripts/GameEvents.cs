6d64222a2f6760ea0c3451a5bc3ad516c6874938
Afua678 <afua.anyadike@gmail.com>
2025-12-03 18:28:57 -0500

    Made the Delegate for Fox

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    // Creates delegate
+    public delegate void Action();
+    public static ifSpacePressed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ifSpacePressed?.Invoke();
+        }
+    }
+}
