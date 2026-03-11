0a081f828ae57c25cbff4a065dc038f56c71aed8
Afua678 <afua.anyadike@gmail.com>
2026-03-11 16:43:07 -0400

    Took out commented code

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -21,34 +21,16 @@ public class GameEvents : MonoBehaviour
     //announce to all subscriber that game is over /game is reset
      public static void TriggerGameOver()
     {
-        // if (OnGameOver != null)
-        // {
-        //     foreach (Action subscriber in OnGameOver.GetInvocationList())
-        //     {
-        //         subscriber.Invoke();
-        //     }
-        // }
+        
         OnGameOver?.Invoke();
-        // for(int i = 0; i<100000; i++)
-        // {
-            
-        // }
-        //  Destroy(null, 3f);
-        //  OnGameReset?.Invoke();
+       
     }
      public static void TriggerGameReset()
     {
-        // if (OnGameReset != null)
-        // {
-        //     foreach (Action subscriber in OnGameReset.GetInvocationList())
-        //         subscriber.Invoke();
-        // }
+      
 
         OnGameReset?.Invoke();
-        //  foreach (var car in GameObject.FindGameObjectsWithTag("Car"))
-        // {
-        //     Destroy(car);
-        // }
+        
     }
 
 
